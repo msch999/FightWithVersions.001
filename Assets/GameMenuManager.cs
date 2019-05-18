a3bd902ddaf4ba9cf26633a34c7c49f4f309d7f5
msch <post_msch-git@yahoo.de>
2019-05-18 19:52:20 +0200

    Button switches off the MainMenu UI

---
+++ Assets/GameMenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuManager : MonoBehaviour
+{
+    public GameObject gamePlay;
+    public GameObject mainUI;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamePlay.SetActive(false);
+    }
+
+    public void HandlePointerClick()
+    {
+        Debug.Log("It's me, HandlePointerClick");
+        gamePlay.SetActive(true);
+        mainUI.SetActive(false);
+    }
+
+}
