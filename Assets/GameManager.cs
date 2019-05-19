e14a1cec35be6b4aa8c1c9b5eef84b8c6bdbe995
msch <post_msch-git@yahoo.de>
2019-05-19 15:09:32 +0200

    Scene loading sort of working

---
+++ Assets/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Chapter4Recipe5
+{
+
+    public class GameManager : MonoBehaviour
+    {
+
+        public static GameManager instance = null;
+
+        private const string SCENE_NAME_01 = "Level1";
+
+        public MenuManager menuManager;
+
+        // Create a singleton
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void OnPlayGameClicked()
+        {
+            Debug.Log("I am OnPlayGame and I am clicked");
+            StartCoroutine(LoadScene(SCENE_NAME_01));
+        }
+
+        IEnumerator LoadScene(string sceneName)
+        {
+            yield return null;
+
+            AsyncOperation asyncOp = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+            asyncOp.allowSceneActivation = false;
+
+            while (!asyncOp.isDone)
+            {
+                float progress = Mathf.Clamp01(asyncOp.progress / 0.9f);
+
+                // Load is complete at 0.9
+                if (asyncOp.progress == 0.9f)
+                {
+                    // Scene loaded, now activate
+                    asyncOp.allowSceneActivation = true;
+
+                }
+
+                yield return null;
+            }
+        }
+
+        public void OnGameOver()
+        {
+            StartCoroutine(UnloadScene());
+            menuManager.OnGameOver();
+        }
+
+        IEnumerator UnloadScene()
+        {
+            yield return SceneManager.UnloadSceneAsync(SCENE_NAME_01);
+
+        }
+    }
+}
