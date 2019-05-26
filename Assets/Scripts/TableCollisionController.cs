036b298fb2fb72ef52d62bf6fc13501ebbfa5ece
msch <post_msch-git@yahoo.de>
2019-05-26 23:57:08 +0200

    TableCollisionController added

---
+++ Assets/Scripts/TableCollisionController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TableCollisionController : MonoBehaviour
+{
+
+    public PancakeScoreBoardController scoreboard;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "PancakeTag")
+        {
+            PancakeFlipped flipped = collider.gameObject.GetComponent<PancakeFlipped>();
+            if (flipped.hasFlipped)
+            {
+                scoreboard.PancakeFlipped();
+            }
+        }
+    }
+}
