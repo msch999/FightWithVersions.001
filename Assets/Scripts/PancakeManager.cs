7e68957a09761748ad37d4b801ecd5c51fdc3cfb
msch <post_msch-git@yahoo.de>
2019-05-26 23:57:08 +0200

    TableCollisionController added

--- Assets/Scripts/PancakeManager.cs
+++ Assets/Scripts/PancakeManager.cs
@@ -1,60 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-namespace chap6FryingPan
+
+public class PancakeManager : MonoBehaviour
 {
 
-    public class PancakeManager : MonoBehaviour
+    public GameObject pancakePrefab;
+    public Transform pancakeDispensePos;
+    public PancakeScoreBoardController scoreboard;
+
+    private bool inGame;
+    private List<GameObject> pancakes;
+
+    void Start()
     {
+        inGame = false;
+        pancakes = new List<GameObject>();
+    }
 
-        public GameObject pancakePrefab;
-        public Transform pancakeDispensePos;
-        public PancakeScoreBoardController scoreboard;
-
-        private bool inGame;
-        private List<GameObject> pancakes;
-
-        void Start()
+    void Update()
+    {
+        //if (GvrController.ClickButtonDown && inGame)
+        if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButtonDown(GvrControllerButton.TouchPadButton) && inGame)
         {
-            inGame = false;
-            pancakes = new List<GameObject>();
+            AddPancake();
         }
-
-        void Update()
+        else if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButtonDown(GvrControllerButton.TouchPadButton) && !inGame)
         {
-            //if (GvrController.ClickButtonDown && inGame)
-            if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButtonDown(GvrControllerButton.TouchPadButton) && inGame)
-            {
-                AddPancake();
-            }
-            else if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButtonDown(GvrControllerButton.TouchPadButton) && !inGame)
-            {
-                Startgame();
-                AddPancake();
-            }
+            Startgame();
+            AddPancake();
         }
+    }
 
-        private void AddPancake()
-        {
-            GameObject pancake = Instantiate(pancakePrefab);
-            pancake.transform.position = pancakeDispensePos.position;
-            pancakes.Add(pancake);
-        }
+    private void AddPancake()
+    {
+        GameObject pancake = Instantiate(pancakePrefab);
+        pancake.transform.position = pancakeDispensePos.position;
+        pancakes.Add(pancake);
+    }
 
-        public void Startgame()
-        {
-            inGame = true;
-            scoreboard.StartGame();
-        }
+    public void Startgame()
+    {
+        inGame = true;
+        scoreboard.StartGame();
+    }
 
-        public void GameOver()
+    public void GameOver()
+    {
+        inGame = false;
+        foreach (GameObject pancake in pancakes)
         {
-            inGame = false;
-            foreach (GameObject pancake in pancakes)
-            {
-                Destroy(pancake);
-            }
+            Destroy(pancake);
         }
     }
 }
