9afb42c064b44a4f8294507af9b68cb7a2783429
msch <post_msch-git@yahoo.de>
2019-05-30 16:34:36 +0200

    Pancake Game sort of working, but flipping is not registered

--- Assets/Scripts/PancakeFlipped.cs
+++ Assets/Scripts/PancakeFlipped.cs
@@ -12,9 +12,11 @@ public class PancakeFlipped : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("This is PancakeFlipped.OnCollisonEnter()");
         if (collision.gameObject.tag == "FryingPanTag")
         {
             bool topBottom = false;
+            Debug.Log("This is OnCollisonEnter(), I encountered a FryingPanTag");
             Vector3 projectedUp = Vector3.Project(transform.up, Vector3.up);
             hasFlipped = projectedUp.y < 0;
         }
--- Assets/Scripts/TableCollisionController.cs
+++ Assets/Scripts/TableCollisionController.cs
@@ -7,11 +7,14 @@ public class TableCollisionController : MonoBehaviour
 
     void OnTriggerEnter(Collider collider)
     {
+        Debug.Log("TableCollisionController: I am OnTriggerEnter()");
         if (collider.gameObject.tag == "PancakeTag")
         {
+            Debug.Log("collider param is tagged PancakeTag");
             PancakeFlipped flipped = collider.gameObject.GetComponent<PancakeFlipped>();
             if (flipped.hasFlipped)
             {
+                Debug.Log("And I have been flipped");
                 scoreboard.PancakeFlipped();
             }
         }
