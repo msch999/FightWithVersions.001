b7dfaef81b1738bbe887128c1a6bfdb40ec3252d
msch <post_msch-git@yahoo.de>
2019-06-01 20:05:07 +0200

    Grenade Mechanic sort of working

---
+++ Assets/Scripts/BombController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+
+    public Material transparentMat;
+    public Material bombMat;
+    public GameObject bombPrefab;
+
+    private static MeshRenderer rend;
+    private Vector3 throwVelocity;
+    private Vector3 previousPosition;
+
+    void Start()
+    {
+        rend = gameObject.GetComponentInChildren<MeshRenderer>();
+        rend.material = transparentMat;
+        Debug.Log("BombController.Start(), rend.material: " + rend.material);
+    }
+
+    void Update()
+    {   // old to new Daydream API
+        //if (GvrController.ClickButtonDown)
+        if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButtonDown(GvrControllerButton.TouchPadButton))
+        {
+            rend.material = bombMat;
+            Debug.Log("BombController.Update() Btn Dwn, rend.material: " + rend.material);
+
+        }  // old to new Daydream API
+        //else if (GvrController.ClickButtonUp)
+        else if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButtonUp(GvrControllerButton.TouchPadButton))
+        {
+            rend.material = transparentMat;
+            Debug.Log("BombController.Update() Btn Up, rend.material: " + rend.material);
+            Vector3 bombPos = gameObject.transform.position;
+            GameObject bombParticle = Instantiate(bombPrefab, bombPos, Quaternion.identity);
+            Rigidbody rb = bombParticle.GetComponent<Rigidbody>();
+            rb.useGravity = true;
+            rb.AddForce(throwVelocity, ForceMode.VelocityChange);
+            BombExplosion explosion = bombParticle.GetComponent<BombExplosion>();
+            explosion.Detonate();
+        }
+
+        //if (GvrController.ClickButton)
+        if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButtonDown(GvrControllerButton.TouchPadButton))
+        {
+            Vector3 currentVelocity = (transform.position - previousPosition) / Time.deltaTime;
+            const int samples = 3;
+            throwVelocity = throwVelocity * (samples - 1) / samples + currentVelocity / samples;
+            previousPosition = transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/BombExplosion.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+public class BombExplosion : MonoBehaviour
+{
+
+    public void Detonate()
+    {
+        Debug.Log("This is BombExplosion.Detonate()");
+        Invoke("ExplodeBomb", 1.5f);
+    }
+
+    private void ExplodeBomb()
+    {
+        float radius = 7.0F;
+        float power = 500.0F;
+
+        Vector3 explosionPos = gameObject.transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(power, explosionPos, 100f, 20.0F);
+            }
+        }
+        Destroy(gameObject);
+    }
+}
