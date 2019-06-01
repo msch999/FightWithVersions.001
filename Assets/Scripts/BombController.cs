28c376fbfd177bb0eb2d69a09e28c1c96697a3a0
msch <post_msch-git@yahoo.de>
2019-06-02 00:01:39 +0200

    Grenade Mechanism working. Throwing faulty

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -7,7 +7,7 @@ public class BombController : MonoBehaviour
     public Material bombMat;
     public GameObject bombPrefab;
 
-    private static MeshRenderer rend;
+    private MeshRenderer rend;
     private Vector3 throwVelocity;
     private Vector3 previousPosition;
 
@@ -15,7 +15,7 @@ public class BombController : MonoBehaviour
     {
         rend = gameObject.GetComponentInChildren<MeshRenderer>();
         rend.material = transparentMat;
-        Debug.Log("BombController.Start(), rend.material: " + rend.material);
+        //Debug.Log("BombController.Start(), rend.material: " + rend.material);
     }
 
     void Update()
@@ -24,27 +24,28 @@ public class BombController : MonoBehaviour
         if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButtonDown(GvrControllerButton.TouchPadButton))
         {
             rend.material = bombMat;
-            Debug.Log("BombController.Update() Btn Dwn, rend.material: " + rend.material);
+            //Debug.Log("BombController.Update() Btn Dwn, rend.material: " + rend.material);
 
         }  // old to new Daydream API
         //else if (GvrController.ClickButtonUp)
         else if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButtonUp(GvrControllerButton.TouchPadButton))
         {
             rend.material = transparentMat;
-            Debug.Log("BombController.Update() Btn Up, rend.material: " + rend.material);
+            //Debug.Log("BombController.Update() Btn Up, rend.material: " + rend.material);
             Vector3 bombPos = gameObject.transform.position;
             GameObject bombParticle = Instantiate(bombPrefab, bombPos, Quaternion.identity);
             Rigidbody rb = bombParticle.GetComponent<Rigidbody>();
-            rb.useGravity = true;
+            //rb.useGravity = true;
             rb.AddForce(throwVelocity, ForceMode.VelocityChange);
             BombExplosion explosion = bombParticle.GetComponent<BombExplosion>();
             explosion.Detonate();
         }
 
         //if (GvrController.ClickButton)
-        if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButtonDown(GvrControllerButton.TouchPadButton))
+        if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButton(GvrControllerButton.TouchPadButton))
         {
             Vector3 currentVelocity = (transform.position - previousPosition) / Time.deltaTime;
+            //Debug.Log("BombController.Update(), current Velocity: " + currentVelocity) ;
             const int samples = 3;
             throwVelocity = throwVelocity * (samples - 1) / samples + currentVelocity / samples;
             previousPosition = transform.position;
--- Assets/Scripts/BombExplosion.cs
+++ Assets/Scripts/BombExplosion.cs
@@ -4,12 +4,23 @@ public class BombExplosion : MonoBehaviour
 
     public void Detonate()
     {
-        Debug.Log("This is BombExplosion.Detonate()");
+        //Debug.Log("This is BombExplosion.Detonate()");
         Invoke("ExplodeBomb", 1.5f);
     }
 
+
+
     private void ExplodeBomb()
     {
+        /*
+            The ExplodeBomb() method uses the Physics.OverlapShere() method to
+            retrieve all the colliders that are within a radius from the position of the explosion.
+            A foreach loop is then used to cycle through each of the colliders, the Rigidbody
+            associated with the collider is accessed, and an explosion force is added to it.
+            AddExplosionForce takes power, position, radius, and upward modifier
+            parameters. The upward modifier describes how much the explosion should lift the
+            objects off the ground.
+        */
         float radius = 7.0F;
         float power = 500.0F;
 
