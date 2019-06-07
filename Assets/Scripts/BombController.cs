c56ab1c4659d56a014f8cb4187ee03e872c6af19
msch <post_msch-git@yahoo.de>
2019-06-08 00:45:04 +0200

    Weapon selector working

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -15,7 +15,7 @@ public class BombController : MonoBehaviour
     {
         rend = gameObject.GetComponentInChildren<MeshRenderer>();
         rend.material = transparentMat;
-        //Debug.Log("BombController.Start(), rend.material: " + rend.material);
+        Debug.Log("BombController.Start(), rend.material: " + rend.material);
     }
 
     void Update()
@@ -24,19 +24,24 @@ public class BombController : MonoBehaviour
         if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButtonDown(GvrControllerButton.TouchPadButton))
         {
             rend.material = bombMat;
-            //Debug.Log("BombController.Update() Btn Dwn, rend.material: " + rend.material);
-
-        }  // old to new Daydream API
+            Debug.Log("BombController.Update() Button Down, rend.material: " + rend.material);
+        } // old to new Daydream API
         //else if (GvrController.ClickButtonUp)
         else if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButtonUp(GvrControllerButton.TouchPadButton))
         {
             rend.material = transparentMat;
-            //Debug.Log("BombController.Update() Btn Up, rend.material: " + rend.material);
             Vector3 bombPos = gameObject.transform.position;
             GameObject bombParticle = Instantiate(bombPrefab, bombPos, Quaternion.identity);
             Rigidbody rb = bombParticle.GetComponent<Rigidbody>();
-            //rb.useGravity = true;
-            rb.AddForce(throwVelocity, ForceMode.VelocityChange);
+            //rb.AddForce(throwVelocity, ForceMode.VelocityChange);
+
+            rb.isKinematic = false; // false = Re-enables the physics engine.
+            rb.useGravity = false;
+            Vector3 throwVector = transform.position - previousPosition; // Get the direction that we're throwing
+            rb.AddForce(throwVector * 10, ForceMode.Force); // Throws the ball by sending a force
+
+
+            //Debug.Log("BombController.Update() Button Up, throwVelocity: " + throwVelocity);
             BombExplosion explosion = bombParticle.GetComponent<BombExplosion>();
             explosion.Detonate();
         }
@@ -45,9 +50,9 @@ public class BombController : MonoBehaviour
         if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButton(GvrControllerButton.TouchPadButton))
         {
             Vector3 currentVelocity = (transform.position - previousPosition) / Time.deltaTime;
-            //Debug.Log("BombController.Update(), current Velocity: " + currentVelocity) ;
             const int samples = 3;
             throwVelocity = throwVelocity * (samples - 1) / samples + currentVelocity / samples;
+            //Debug.Log("BombController.Update(), GetButton, current Velocity: " + currentVelocity + " throwVelocity: " + throwVelocity);
             previousPosition = transform.position;
         }
     }
---
+++ Assets/Scripts/WeaponSelector.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class WeaponSelector : MonoBehaviour
+{
+
+    enum Weapon { GUN, BOMB };
+
+    public GameObject gun;
+    public GameObject bomb;
+
+    private Weapon currentWeapon;
+
+    void Start()
+    {
+        currentWeapon = Weapon.GUN;
+        gun.SetActive(true);
+        bomb.SetActive(false);
+        Debug.Log("WeaponSelector.Start(), gun active");
+    }
+
+    void Update()
+    {
+        //if (GvrController.AppButtonDown)
+        if (GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButtonDown(GvrControllerButton.App))
+        {
+            if (currentWeapon == Weapon.GUN)
+            {
+                gun.SetActive(false);
+                bomb.SetActive(true);
+                currentWeapon = Weapon.BOMB;
+                Debug.Log("WeaponSelector.Update(), bomb active");
+            }
+            else if (currentWeapon == Weapon.BOMB)
+            {
+                gun.SetActive(true);
+                bomb.SetActive(false);
+                currentWeapon = Weapon.GUN;
+                Debug.Log("WeaponSelector.Update(), gun active");
+            }
+        }
+    }
+}
